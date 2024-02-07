eab947d34b099c19cc9930ecdd2363b90f410f04
Nvvme <144735410+Nvvme@users.noreply.github.com>
2024-02-07 11:30:59 -0500

    Add files via upload
    added files

---
+++ js/page-color-changer.js
@@ -0,0 +1,8 @@
+const redSlider = document.querySelector("#red");
+const greenSlider = document.querySelector("#green");
+const blueSlider = document.querySelector("#blue");
+//const body = document.querySelector("body");
+
+function colorupdate() {
+    
+}
