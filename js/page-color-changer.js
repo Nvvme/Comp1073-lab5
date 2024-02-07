a31b62d92080da7ff6de03b9f6236e9b128c8dc4
unknown <navan2400@gmail.com>
2024-02-07 12:21:05 -0500

    it still doesn't work

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -1,8 +1,23 @@
+//declaring Constents
 const redSlider = document.querySelector("#red");
 const greenSlider = document.querySelector("#green");
 const blueSlider = document.querySelector("#blue");
-//const body = document.querySelector("body");
+const body = document.querySelector("body");
 
-function colorupdate() {
-    
+//creating the function
+//even listeners
+//remembered to use CamelCase
+function colorUpdate() {
+    const redValue = redSlider.getAttribute("value");
+    const greenValue = greenSlider.getAttribute("value");
+    const blueValue = blueSlider.getAttribute("value");
+    const rgbValue = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+// used `` 
+    body.style.backgroundColor = rgbValue;
 }
+
+redSlider.addEventListener("input", colorUpdate);
+greenSlider.addEventListener("input", colorUpdate);
+blueSlider.addEventListener("input", colorUpdate);
+
+colorUpdate();
